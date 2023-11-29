1b60fd71306ee58519bc06646945a81e87b04625
jellyfish-bit <109416703+jellyfish-bit@users.noreply.github.com>
2023-11-29 11:31:11 +0100

    Update script.js
    Added check for December

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const day = new Date().getDate();
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth();
 console.info("Current Day: " + day)
+console.info("Current Month: " + month)
 
 for (let index = 0; index < 24; index++) {
-    if(day < index+1) {
+    if(day < index+1 && month !== 11) {
         document.getElementById("day" + (index+1)).disabled = true;
     }
 }
