360744722a362535167ddb43c3154bb0b532ce4f
jellyfish-bit <109416703+jellyfish-bit@users.noreply.github.com>
2023-11-29 12:43:12 +0100

    Update script.js
    Minor fix

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.info("Current Day: " + day)
 console.info("Current Month: " + month)
 
 for (let index = 0; index < 24; index++) {
-    if(day < index+1 && month !== 11) {
+    if(day < index+1 || month !== 11) {
         document.getElementById("day" + (index+1)).disabled = true;
     }
 }
